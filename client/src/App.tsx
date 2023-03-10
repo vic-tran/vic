889300af5d1edcfa6a7494903341565517819b55
Victor Tran <vc.tran998@gmail.com>
2023-03-10 15:43:25 -0800

    Todo list functionality

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,79 @@
 import useSWR from 'swr';
 import './App.css'
-import { Box } from '@mantine/core'
+import { Box, List, ThemeIcon } from '@mantine/core'
+import AddTodo from './components/AddTodo'
+import { CheckCircleFillIcon } from '@primer/octicons-react';
+
+export interface Todo {
+  id: number;
+  title: string;
+  body: string;
+  done: boolean;
+
+}
 
 export const ENDPOINT = 'http://localhost:4000'
 
 const fetcher = (url: string) =>
-  fetch(`${ENDPOINT}/${url}`).then((r) => r.json());
-
+  fetch(`${ENDPOINT}/${url}`)
+    .then((r) => {
+      if (!r.ok) {
+        throw new Error(`Failed to fetch ${url}: ${r.statusText}`);
+      }
+      return r.json();
+    })
+    .catch((err) => {
+      console.error(err);
+      throw err;
+    });
+    
 function App () {
+  const { data, mutate } = useSWR<Todo[]>("api/todos", fetcher);
+
+  async function markTodoAsDone(id: number){
+    const updated = await fetch (`${ENDPOINT}/api/todos/${id}/done`, {
+      method: "PATCH",
+    }).then((r) => r.json());
+
+    mutate(updated);
+  }
 
   return (
-   <Box>
-      Hello World
+   <Box
+    sx= {(theme) => ({
+      padding: "2rem",
+      width: "100%",
+      maxWidth: "40rem",
+      margin: "0 auto",
+    })}
+   >
+
+    <List spacing = "xs" size ="sm" mb={12} center>
+      {data?.map((todo) => {
+        return ( 
+        <List.Item 
+        onClick={() => markTodoAsDone(todo.id)}
+        key={`todo_list__${todo.id}`}
+        icon = {
+          todo.done ? (
+          <ThemeIcon color = "teal" size={24} radius="xl">
+            <CheckCircleFillIcon size={20}/>
+          </ThemeIcon>
+          ) : (
+            <ThemeIcon color = "gray" size={24} radius="xl">
+            <CheckCircleFillIcon size={20}/>
+          </ThemeIcon> 
+          )
+         }
+        >
+          {todo.title}
+          </List.Item>
+          );
+      })}
+    </List>
+      <AddTodo mutate={mutate}/>
    </Box>
   )
 }
 
-export default App
+export default App;
--- server/main.go
+++ server/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 
 )
 
@@ -18,6 +19,11 @@ func main() {
 	fmt.Print("Hello world")
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	todos := []Todo{}
 
